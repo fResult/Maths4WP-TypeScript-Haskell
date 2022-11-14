7ae16149b9499645c7f22938b10802a66cdfeddb
fResult <styxmaz@gmail.com>
2022-11-15 02:51:32 +0700

    Add Readme file

---
+++ README.md
@@ -0,0 +1,7 @@
+# My Practice from Mathematics for Working Programmer 2.0 class
+...
+## Curried Functions
+Day 2: Slide 425-475
+
+## Generic Functions
+Day 2: Slide 481-xxx
---
+++ curried-functions/cat-by-id.ts
@@ -0,0 +1,52 @@
+/*
+ * This file is implemented follow concept in Maths4WP
+ * Day 2: Currying (Slide 421-475)
+ */
+type Cat = string;
+type DbOptions = { where: { id: string } };
+
+const myDB = {
+    get(table: string, { where }: DbOptions): Cat {
+        return `${table}-${where.id}`;
+    },
+};
+const myAPI = {
+    get(id: string): Cat {
+        return `cats/${id}`;
+    },
+};
+function mockGetCat(id: string): Cat {
+    return `mockGetCat-${id}`;
+}
+
+function getCatFromDB(db: typeof myDB) {
+    return function getCatByID(catID: string) {
+        return db.get("cats", {
+            where: {
+                id: catID,
+            },
+        });
+    };
+}
+function getCatFromAPI(api: typeof myAPI) {
+    return function getCatByID(catID: string): Cat {
+        return api.get(catID);
+    };
+}
+
+function catByID(getCatByID: (catID: string) => Cat) {
+    return function byID(id: string): Cat {
+        return getCatByID(id);
+    };
+}
+
+const cat1 = catByID(getCatFromDB(myDB))("17");
+const cat2 = catByID(getCatFromAPI(myAPI))("13");
+// Of course, it (pointlessly) works with itself
+const cat3 = catByID(catByID(getCatFromAPI(myAPI)))("19");
+const cat4 = catByID(mockGetCat)("21");
+
+console.log("From DB", cat1); // From DB cats-17
+console.log("From API", cat2); // From API cats/13
+console.log("From API", cat3); // From API cats/19
+console.log("From Mock", cat4); // From Mock mockGetCat-21
