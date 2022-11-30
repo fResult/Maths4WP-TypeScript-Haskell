5a210510deb59cc645d76cb236f87fc1ef57fdec
fResult <styxmaz@gmail.com>
2022-12-01 01:10:15 +0700

    Try my custom filter' and map' functions

--- haskell/functions/higher-order-functions.hs
+++ haskell/functions/higher-order-functions.hs
@@ -9,3 +9,11 @@ twice' f x = (f.f) x
 
 thrice' :: (a -> a) -> a -> a
 thrice' f x = (f.f.f) x
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ [] = []
+filter' predicate xs = [x | x <- xs, predicate x]
+
+map' :: (a -> b) -> [a] -> [b]
+map' _ [] = []
+map' mapper (x:xs) = mapper x : map' mapper xs
