735c4e8acb448b44e43285e3f69fc57b3c823867
fResult <styxmaz@gmail.com>
2022-12-05 00:21:47 +0700

    Try Shape Datatypes

--- haskell/types/custom-types.hs
+++ haskell/types/custom-types.hs
@@ -1,1 +1,13 @@
-data Day = Sun | Mon | Tue | Wed | Thu | Fri | Sat deriving Show
+-- Enum
+data Day = Sun | Mon | Tue | Wed | Thu | Fri | Sat
+    deriving Show
+-- day = Sat :: Day
+-- :t day = Day
+-- show day -- "Sat"
+
+data Shape = Circle Float | Square Float
+    deriving Show
+-- Circle 20    -- 20.0
+-- Square 20    -- 20.0
+-- :t Circle    -- Float -> Shape
+-- :t Circle 20 -- Shape
