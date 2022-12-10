42a555d8e788d03fdd27c3e78ca01819206e8e50
fResult <styxmaz@gmail.com>
2022-12-10 23:38:25 +0700

    Implement StackRight

--- haskell/types/generics-types/stack-queue.hs
+++ haskell/types/generics-types/stack-queue.hs
@@ -1,6 +1,6 @@
--- import Data.List (delete, last)
+import Data.List (delete, last)
 
-{-# StackLeft Implementation #-}
+{- StackLeft Implementation START -}
 data StackLeft a = StackLeft [a] deriving Show
 
 createStackLeft :: [a] -> StackLeft a
@@ -19,7 +19,29 @@ emptyStackLeft (StackLeft _) = StackLeft []
 topStackLeft :: StackLeft a -> a
 topStackLeft (StackLeft []) = error "Can't top empty Stack"
 topStackLeft (StackLeft (x:_)) = x
+{- StackLeft Implementation END -}
 
+{- StackRight Implementation START -}
 data StackRight a = StackRight [a] deriving Show
+
+createStackRight :: [a] -> StackRight a
+createStackRight xs = StackRight xs
+
+pushStackRight :: a -> StackRight a -> StackRight a
+pushStackRight x (StackRight xs) = StackRight (xs ++ [x])
+
+popStackRight :: (Eq a) => StackRight a -> StackRight a
+popStackRight (StackRight []) = error "Can't pop empty stack"
+popStackRight (StackRight xs) = StackRight (delete (last xs) xs)
+
+topStackRight :: StackRight a -> a
+topStackRight (StackRight []) = error "Can't top empty stack"
+topStackRight (StackRight xs) = last xs
+
+emptyStackRight :: StackRight a -> StackRight a
+emptyStackRight (StackRight _) = StackRight []
+
+{- StackRight Implementation END -}
+
 data QueueLeft a = QueueLeft [a] deriving Show
 data QueueRight a = QueueRight [a] deriving Show
