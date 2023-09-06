2ff100ddee608ac9d56c3e0a01a70b86289c8f15
Korn Sila <sila@magicboxsolution.com>
2023-09-06 21:45:16 +0700

    Try implement foldl

--- haskell/functions/higher-order-functions.hs
+++ haskell/functions/higher-order-functions.hs
@@ -30,3 +30,7 @@ foldr' :: (a -> b -> b) -> b -> [a] -> b
 foldr' _ b []     = b
 foldr' f b (x:xs) = f x (foldr' f b xs)
 -- foldr' (+) 0 [1..10] = 55
+
+foldl' :: (b -> a -> b) -> b -> [a] -> b
+foldl' _ b []     = b
+foldl' f b (x:xs) = foldl' f (f b x) xs
