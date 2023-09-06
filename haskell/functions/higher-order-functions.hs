fd309520db90f09653a37540b206550b9c838422
Korn Sila <sila@magicboxsolution.com>
2023-09-06 21:51:52 +0700

    Try using foldl as map

--- haskell/functions/higher-order-functions.hs
+++ haskell/functions/higher-order-functions.hs
@@ -29,8 +29,22 @@ filter'' predicate (x:xs)
 foldr' :: (a -> b -> b) -> b -> [a] -> b
 foldr' _ b []     = b
 foldr' f b (x:xs) = f x (foldr' f b xs)
--- foldr' (+) 0 [1..10] = 55
 
 foldl' :: (b -> a -> b) -> b -> [a] -> b
 foldl' _ b []     = b
 foldl' f b (x:xs) = foldl' f (f b x) xs
+
+-- map' (*2) [1..5]
+---- [2,4,6,8,10]
+
+-- foldr' ((:) . (*2)) [] [1..5]
+---- [2,4,6,8,10]
+
+-- foldl' ((:) . (*2)) [] [1..5]
+---- ERROR
+
+-- :t flip
+---- flip :: (a -> b -> c) -> b -> a -> c
+
+-- foldl' (flip ((:) . (*2))) [] [1..5]
+---- [10,8,6,4,2]
