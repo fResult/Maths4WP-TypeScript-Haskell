be16b1b91f8684c6584a42f32bafbe15bb45cdca
Korn Sila <sila@magicboxsolution.com>
2023-09-09 15:56:12 +0700

    Add deliberately example for imcomputable by Exhaustive Pattern

--- haskell/math-hacks/incomputable.hs
+++ haskell/math-hacks/incomputable.hs
@@ -5,3 +5,14 @@ to :: a -> Bool
 to be = not (to be)
 -- to "Something"
 ---- Run forever
+
+-- head []
+---- *** Exception: Prelude.head: empty list
+
+-- 7 `div` 0
+---- *** Exception: divide by zero
+
+partial :: Bool -> Bool
+partial True = True
+-- partial False
+---- *** Exception: Non-exhaustive patterns in function partial
