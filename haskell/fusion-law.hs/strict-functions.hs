31b9bf3c58f2b1fa1c66ab6c7ed8813fa28cc865
Korn Sila <sila@magicboxsolution.com>
2023-09-10 05:56:50 +0700

    Prepare proven of strict function of the Fusion Law

--- haskell/fusion-law.hs/strict-functions.hs
+++ haskell/fusion-law.hs/strict-functions.hs
@@ -1,2 +1,10 @@
 module StrictFunctions where
 
+--- Prove Strict Functions ---
+---- foldr f' a' . map g'                     <-- 1
+--            (f . foldr g a) = f h b         <-- 2
+--            (f . foldr ((:) . g') [])
+
+---- map g' = foldr ((:) . g') []             <-- 1
+---- g = ((:) . g'), a = []                   <-- 2
+---- f = foldr f' a'                          <-- 1, 2
