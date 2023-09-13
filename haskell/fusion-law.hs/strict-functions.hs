f94381df8670834018f8ff7d2ffbfd9e335d2822
Korn Sila <sila@magicboxsolution.com>
2023-09-14 01:00:28 +0700

    Try doing directly derived from fusion law

--- haskell/fusion-law.hs/strict-functions.hs
+++ haskell/fusion-law.hs/strict-functions.hs
@@ -8,3 +8,16 @@ module StrictFunctions where
 ---- map g' = foldr ((:) . g') []             <-- 1
 ---- g = ((:) . g'), a = []                   <-- 2
 ---- f = foldr f' a'                          <-- 1, 2
+
+double :: Int -> Int
+double = (*2)
+-- double . sum $ [1..10]
+---- 110
+-- sum . map double $ [1..10]
+---- 110
+-- foldr (+) 0 . map double $ [1..10]
+---- 110
+-- foldr ((+) . double) 0 $ [1..10]
+---- 110
+
+-- foldr f a . map g = foldr (f . g) a
