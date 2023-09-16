a76c108a6a7170c05e088976464b974dd6b3b815
Korn Sila <sila@magicboxsolution.com>
2023-09-16 19:59:26 +0700

    Add the case for NOT EMPTY LIST

--- haskell/optimize-by-eqution-reasoning/scanl-problem.hs
+++ haskell/optimize-by-eqution-reasoning/scanl-problem.hs
@@ -21,7 +21,7 @@ scanl' f e = (map (foldl f e)) . inits
 --                      = [0,1,3,6]
 
 --- DO Equational Reasoning of Scanl ---
----- CASE []
+---- CASE [], let's assume...
 ---- scanl f e [] = (map (foldl f e) . inits) []
 ----              = map (foldl f e) (inits [])             <-- (g . f) x = g (f x)
 ----              = map (foldl f e) [[]]                   <-- inits [] = [[]]
@@ -30,3 +30,7 @@ scanl' f e = (map (foldl f e)) . inits
 ----              = [foldl f e []]                         <-- x : [] = [x]
 ----              = [e]                                    <-- foldl f b [] = b
 ------      ∴ scanl f e [] = [e]      ------
+---- CASE (x:xs), let's assume...
+---- scanl f e xs     = map (foldl f e) . (inits xs)
+---- scanl f e (x:xs) = map (foldl f e) . (inits (x:xs))
+---- scanl f e (x:xs) = map (foldl f e) . ([] : map (x:) (inits xs))       <-- inits (x:xs) = [] : map (x:) (inits xs)
