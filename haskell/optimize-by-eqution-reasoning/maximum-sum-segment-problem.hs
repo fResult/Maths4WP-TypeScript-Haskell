973985fd780ea7ca49fdbaaeae5b9eac6b06c9c4
Korn Sila <sila@magicboxsolution.com>
2023-09-16 21:00:45 +0700

    Prepare tails and inits function for Maximum Sum Segment Problem

---
+++ haskell/optimize-by-eqution-reasoning/maximum-sum-segment-problem.hs
@@ -0,0 +1,9 @@
+module MaximumSumSegmentProblem where
+
+tails :: [a] -> [[a]]
+tails []       = [[]]
+tails s@(x:xs) = s : tails xs
+
+inits :: [a] -> [[a]]
+inits []     = [[]]
+inits (x:xs) = [] : map (x:) (inits xs)
