7f15f789c2a670417fb35b6bfdbcb729919bc69e
Korn Sila <sila@magicboxsolution.com>
2023-09-19 09:22:04 +0700

    Finish doing proven inits' function

--- haskell/optimize-by-eqution-reasoning/maximum-sum-segment-problem.hs
+++ haskell/optimize-by-eqution-reasoning/maximum-sum-segment-problem.hs
@@ -1,13 +1,24 @@
 module MaximumSumSegmentProblem where
 import Data.List as L (nub)
 
-tails :: [a] -> [[a]]
-tails []       = [[]]
-tails s@(x:xs) = s : tails xs
+tails' :: [a] -> [[a]]
+tails' []       = [[]]
+tails' s@(x:xs) = s : tails' xs
 
-inits :: [a] -> [[a]]
-inits []     = [[]]
-inits (x:xs) = [] : map (x:) (inits xs)
+inits' :: [a] -> [[a]]
+inits' []     = [[]]
+inits' (x:xs) = [] : map (x:) (inits' xs)
 
-segments :: Eq a => [a] -> [[a]]
-segments = nub . concat . map inits . tails
+segments' :: Eq a => [a] -> [[a]]
+segments' = nub . concat . map inits' . tails'
+
+-- inits' [1,2,3] = [] : map (1:) (inits' [2,3])
+--               = [] : map (1:) ([] : map (2:) (inits' [3]))
+--               = [] : map (1:) ([] : map (2:) ([] : map (3:) (inits' [])))
+--               = [] : map (1:) ([] : map (2:) ([] : map (3:) [[]]))
+--               = [] : map (1:) ([] : map (2:) ([] : [[3]]))
+--               = [] : map (1:) ([] : map (2:) [[], [3]])
+--               = [] : map (1:) ([] : [[2], [2, 3]])
+--               = [] : map (1:) [[], [2], [2, 3]]
+--               = [] : [[1], [1, 2], [1, 2, 3]]
+--               = [[], [1], [1, 2], [1, 2, 3]]
