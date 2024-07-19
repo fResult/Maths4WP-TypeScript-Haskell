d2fbfd8341e123176d7da67419555bee396e7a6d
fResult <styxmaz@gmail.com>
2024-07-20 00:12:35 +0700

    add day 2 homework 3

--- haskell/crash-course/day-2/homework.hs
+++ haskell/crash-course/day-2/homework.hs
@@ -22,6 +22,15 @@ areaSquare square = areaRectangle Rectangle {height = s, weight = s}
   where
     s = side square
 
+areaDonut :: Circle -> Circle -> Float
+areaDonut c1 c2 = abs $ circleArea1 - circleArea2
+  where
+    circleArea1 = areaCircle c1
+    circleArea2 = areaCircle c2
+
+c5 = Circle { radius =5}
 c10 = Circle {radius = 10}
+c20 = Circle {radius = 20}
 
 sq10 = Square {side = 10}
+sq20 = Square {side = 20}
