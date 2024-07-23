0e7584df061227a934c1a6bf12ff7f3a8642a59d
fResult <styxmaz@gmail.com>
2024-07-23 20:04:53 +0700

    update day-3's scratch file

--- haskell/crash-course/day-3/scrach-before.hs
+++ haskell/crash-course/day-3/scrach-before.hs
@@ -1,8 +1,23 @@
 sqr :: Float -> Float
 sqr n = n * n
 
-areaCircle :: Float -> Float
-areaCircle r = pi * sqr r
+newtype Circle = Circle Float
+  deriving (Show, Eq)
 
-areaSquare :: Float -> Float
-areaSquare = sqr
+newtype Square = Square Float
+  deriving (Show, Eq)
+
+class HasArea a where
+  area :: a -> Float
+
+instance HasArea Circle where
+  area :: Circle -> Float
+  area (Circle r) = pi * sqr r
+
+instance HasArea Square where
+  area :: Square -> Float
+  area (Square s) = sqr s
+
+type FullName = String
+type NickName = String
+data Person = Person FullName NickName deriving (Show)
