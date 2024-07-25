283c50365ed66c90f0f56a53750e375d16a7e220
fResult <styxmaz@gmail.com>
2024-07-25 12:44:26 +0700

    update scratch file for homework

--- haskell/crash-course/day-3/scratch-after.hs
+++ haskell/crash-course/day-3/scratch-after.hs
@@ -1,41 +1,80 @@
-newtype Circle = Circle {radius :: Float} deriving (Show, Eq)
-newtype Square = Square {side :: Float} deriving (Show, Eq)
-
-data Rectangle = Rectangle {weight :: Float, height :: Float}
-  deriving (Show, Eq)
-
-data Donut = Donut {outerCircle :: Circle, innerCircle :: Circle}
-  deriving (Show, Eq)
+-- {-# LANGUAGE FlexibleInstances #-}
+-- {-# LANGUAGE MultiParamTypeClasses #-}
 
 sqr :: Float -> Float
 sqr n = n * n
+
+half :: Float -> Float
+half = (/ 2)
+
+hypotenuse :: Float -> Float -> Float
+hypotenuse a b = sqrt $ sqr a + sqr b
+
+hypotenuseEq :: Float -> Float
+hypotenuseEq a = hypotenuse a a
+
+newtype Circle = Circle {_circleRadius :: Float} deriving (Show, Eq)
+newtype Square = Square {_squareSide :: Float} deriving (Show, Eq)
+
+data Rectangle = Rectangle {_rectWeight :: Float, _reactHeight :: Float}
+  deriving (Show, Eq)
+
+data Donut = Donut {_donutOuterCircle :: Circle, _donutInnerCircle :: Circle}
+  deriving (Show, Eq)
+
+data Shape = Shape
+  { _shapeCaseSquare :: Square
+  , _shapeInnerCircle :: Circle
+  , _shapeInnerSquare :: Square
+  }
+  deriving (Show)
 
 class HasArea a where
   area :: a -> Float
 
 instance HasArea Circle where
   area :: Circle -> Float
-  area c = pi * sqr (radius c)
+  area c = pi * sqr (_circleRadius c)
 
 instance HasArea Rectangle where
   area :: Rectangle -> Float
-  area rect = weight rect * height rect
+  area rect = _rectWeight rect * _reactHeight rect
 
 instance HasArea Square where
   area :: Square -> Float
   area sq = area $ Rectangle s s
    where
-    s = side sq
+    s = _squareSide sq
 
 instance HasArea Donut where
   area :: Donut -> Float
   area donut = area outCirc - area inCirc
    where
-    outCirc = outerCircle donut
-    inCirc = innerCircle donut
+    outCirc = _donutOuterCircle donut
+    inCirc = _donutInnerCircle donut
+
+instance HasArea Shape where
+  area :: Shape -> Float
+  area shp = area innerCircle - area innerSquare
+   where
+    innerCircle = _shapeInnerCircle shp
+    innerSquare = _shapeInnerSquare shp
 
 type Radius = Float
 mkDonut :: Radius -> Radius -> Donut
 mkDonut r1 r2
   | r1 > r2 = Donut (Circle r1) (Circle r2)
   | otherwise = Donut (Circle r2) (Circle r1)
+
+type ShapeFrameSide = Float
+mkShape :: ShapeFrameSide -> Shape
+mkShape frameSize =
+  Shape
+    (Square frameSize)
+    (Circle halfFrameSide)
+    (Square innerSquareSide)
+ where
+  innerSquareSide = hypotenuseEq halfFrameSide
+  halfFrameSide = half frameSize
+
+shp = mkShape 100
