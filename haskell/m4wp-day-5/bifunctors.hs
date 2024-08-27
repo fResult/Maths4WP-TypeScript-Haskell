3ba7ac23b05e65d6e6b4d0c7d58de97244d53c32
fResult <styxmaz@gmail.com>
2024-08-28 02:20:56 +0700

    try bifunctor with either

--- haskell/m4wp-day-5/bifunctors.hs
+++ haskell/m4wp-day-5/bifunctors.hs
@@ -1,13 +1,22 @@
+import Data.Bifunctor
+
+{-- Doing Algebra --}
 -- even   :: Int -> Bool
 -- length :: [a] -> Int
 
--- bimap  :: (a -> b) -> (c -> d) -> p a c -> p b d
----- (Int -> Bool) -> ([a] -> Int) -> p Int [a] -> p Bool Int
+-- bimap :: (a -> b) -> (c -> d) -> p a c -> p b d
+----     :: (Int -> Bool) -> ([a] -> Int) -> p Int [a] -> p Bool Int
 
+-- So...
 -- bimap even length :: Bifunctor p => p Int [a] -> p Bool Int
 
 -- Test
--- bimap even length (Left 42)          -- True
--- bimap even length (Left 9)           -- False
--- bimap even length (Right ['a'..'z']) -- 26
--- bimap even length (Right [0..14])    -- 15
+mapEither :: (Bifunctor p) => p Int [a] -> p Bool Int
+mapEither = bimap even length
+
+main :: IO ()
+main = do
+  putStrLn $ "mapEither even length (Left 42)          -- " ++ show (mapEither (Left 42))
+  putStrLn $ "mapEither even length (Left 9)           -- " ++ show (mapEither (Left 9))
+  putStrLn $ "mapEither even length (Right ['a'..'z']) -- " ++ show (mapEither (Right ['a' .. 'z']))
+  putStrLn $ "mapEither even length (Right [0..14])    -- " ++ show (mapEither (Right [0 .. 14]))
