d9413f4654f3408d6adcac739486678d4e0040ed
fResult <styxmaz@gmail.com>
2024-10-12 00:43:06 +0700

    natural transform

--- haskell/m4wp-day-6/natural-transformations.hs
+++ haskell/m4wp-day-6/natural-transformations.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 maybeToList :: Maybe a -> [a]
 maybeToList Nothing = []
 maybeToList (Just x) = [x]
@@ -14,3 +15,53 @@ main1 = do
   putStrLn $ "(maybeToList . fmap even) Nothing: " ++ show (maybeToList . fmap even $ Nothing)
   putStrLn $ "(fmap even . maybeToList) (Just 10): " ++ show (fmap even . maybeToList $ Just 10)
   putStrLn $ "(maybeToList . fmap even) (Just 10): " ++ show (maybeToList . fmap even $ Just 10)
+
+f :: Int -> Bool
+f = undefined
+-- f = even
+-- f = odd
+-- f = (> 10)
+-- f = isPrime
+
+natural :: (Functor f, Functor g) => f a -> g a
+natural = undefined
+-- natural = reverse
+-- natural = tail
+-- natural = seqList
+-- natural = cycle
+
+-- n1 :: [Int] -> [Bool]
+-- n1 = fmap f . natural
+-- n2 :: [Int] -> [Bool]
+-- n2 = natural . fmap f
+-- -- n1 and n2 are commutative
+
+ints2Ints :: [Int] -> [Int]
+ints2Ints = undefined
+
+bools2Bools :: [Bool] -> [Bool]
+bools2Bools = undefined
+
+natural' :: (Functor f, Functor g) => f Int -> g Int
+natural' = undefined
+
+nX :: (Functor f, Functor g) => f Int -> f Bool
+nX = fmap f . natural
+
+nY :: (Functor f, Functor g) => f Int -> g Bool
+nY = natural . fmap f
+
+f' :: Int -> Int
+f' = undefined
+
+nX' :: (Functor f, Functor g) => f Int -> f Int
+nX' = fmap f' . natural
+
+nY' :: (Functor f, Functor g) => f Int -> g Int
+nY' = natural . fmap f'
+
+nX'' :: (Functor f, Functor g) => f Int -> f Int
+nX'' = fmap f' . natural'
+
+nY'' :: (Functor f, Functor g) => f Int -> g Int
+nY'' = natural' . fmap f'
