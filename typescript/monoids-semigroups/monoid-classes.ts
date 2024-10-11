1578de79cb4fa38ddda999e70d13d4f009b98305
fResult <styxmaz@gmail.com>
2024-10-12 02:49:42 +0700

    add new monoid `List`

--- typescript/monoids-semigroups/monoid-classes.ts
+++ typescript/monoids-semigroups/monoid-classes.ts
@@ -19,6 +19,50 @@
         }
     }
 
+    class Add extends Monoid<number> {
+        private constructor(protected x: number) {
+            super(x)
+        }
+
+        public static just(x: number): Add {
+            return new Add(x)
+        }
+
+        public concat(other: Add): Add {
+            return Add.just(this.x + other.x)
+        }
+
+        public static empty() {
+            return Add.just(0)
+        }
+
+        public toString(): string {
+            return `${Add.name}(${this.x})`
+        }
+    }
+
+    class Multiply extends Monoid<number> {
+        private constructor(protected x: number) {
+            super(x)
+        }
+
+        public static just(x: number): Multiply {
+            return new Multiply(x)
+        }
+
+        public concat(other: Multiply): Multiply {
+            return Multiply.just(this.x * other.x)
+        }
+
+        public static empty() {
+            return Multiply.just(1)
+        }
+
+        public toString(): string {
+            return `${Multiply.name}(${this.x})`
+        }
+    }
+
     class Min extends Monoid<number> {
         private constructor(protected x: number) {
             super(x)
@@ -28,18 +72,16 @@
             return new Min(x)
         }
 
-        public concat(other: this): Monoid<number> {
-            return (this.x < other.x
-                ? new Min(this.x)
-                : new Min(other.x)) as unknown as Monoid<number>
-        }
-
-        public toString(): string {
-            return `${Min.name}(${this.x})`
+        public concat(other: Min): Min {
+            return this.x < other.x ?  Min.just(this.x): Min.just(other.x)
         }
 
         public static empty() {
             return Min.just(Infinity)
+        }
+
+        public toString(): string {
+            return `${Min.name}(${this.x})`
         }
     }
 
@@ -52,21 +94,44 @@
             return new Max(x)
         }
 
-        public concat(other: this): Monoid<number> {
-            return (this.x > other.x
-                ? Max.just(this.x)
-                : Max.just(other.x)) as unknown as Monoid<number>
-        }
-
-        public toString(): string {
-            return `${Min.name}(${this.x})`
+        public concat(other: Max): Max {
+            return this.x > other.x ? Max.just(this.x) : Max.just(other.x)
         }
 
         public static empty(): Max {
             return Max.just(-Infinity)
         }
+
+        public toString(): string {
+            return `${Max.name}(${this.x})`
+        }
     }
 
-    console.log(Min.just(12).concat(Min.just(5)).concat(Min.empty()).concat(Min.empty()))
-    console.log(Max.just(12).concat(Max.just(5)).concat(Max.just(15)).concat(Max.empty()))
+    class List<T = any> extends Monoid<T[]> {
+        private constructor(protected x: T[]) {
+            super(x)
+        }
+
+        public static just<T>(xs: T[]): List<T> {
+            return new List(xs)
+        }
+
+        public concat(other: List<T>): List<T> {
+            return List.just(this.x.concat(other.x))
+        }
+
+        public static empty<T>(): List<T> {
+            return List.just([])
+        }
+
+        public toString(): string {
+            return `${List.name}(${this.x.join(", ")})`
+        }
+    }
+
+    console.log(Add.just(1).concat(Add.just(2)).concat(Add.just(3)).concat(Add.empty()).toString())
+    console.log(Multiply.just(12).concat(Multiply.just(5)).concat(Multiply.empty()).toString())
+    console.log(Min.just(12).concat(Min.just(5)).concat(Min.empty()).concat(Min.empty()).toString())
+    console.log(Max.just(12).concat(Max.just(5)).concat(Max.just(15)).concat(Max.empty()).toString())
+    console.log(List.just(["A", "B"]).concat(List.just(["B", "C"])).concat(List.empty()).concat(List.just(["C", "D"])).toString())
 })()
