0d76bcd818569704bd919210c5544630ca1f2862
fResult <styxmaz@gmail.com>
2025-11-10 08:23:28 +0700

    Add `intersect'` implementation using HashMap.

--- haskell/crash-course-2025/day-2/ex-5.hs
+++ haskell/crash-course-2025/day-2/ex-5.hs
@@ -1,2 +1,11 @@
+import Data.Char (isAlpha, toLower)
+import qualified Data.Map as Map
+
 areAnagram :: String -> String -> Bool
-areAnagram = undefined
+areAnagram s1 s2 = elemsFreqMap cleaned1 == elemsFreqMap cleaned2
+  where cleaned1 = cleanStr s1
+        cleaned2 = cleanStr s2
+        cleanStr = map toLower . filter isAlpha
+
+elemsFreqMap :: (Eq a, Ord a) => [a] -> Map.Map a Int
+elemsFreqMap xs = Map.fromListWith (+) [(e, 1) | e <- xs]
