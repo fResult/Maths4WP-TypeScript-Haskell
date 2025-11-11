ee8fae7f3982bc00382ffeb6cc21971fee4dd766
fResult <styxmaz@gmail.com>
2025-11-12 00:47:24 +0700

    Add `sum'` and `count'` functions.

--- haskell/crash-course-2025/day-2/main.hs
+++ haskell/crash-course-2025/day-2/main.hs
@@ -4,3 +4,25 @@ manutdLegend 11 = "Ryan Giggs"
 manutdLegend 1 = "Edwin van der Saar"
 manutdLegend _ = "No Legend"
 
+-- sum' :: Num a => [a] -> a
+sum' :: [Int] -> Int
+sum' []     = 0
+sum' [x]    = x
+sum' (x:xs) = x + sum' xs
+
+count :: [a] -> Int
+count []     = 0
+count [_]    = 1
+count (x:xs) = 1 + count xs
+
+-- λ> count "hello"
+-- 5
+-- λ> count [1..10]
+-- 10
+-- λ> import Data.Char
+-- λ> :type [isSpace, isDigit, isLetter]
+-- λ> [isSpace, isDigit, isLetter] :: [Char -> Bool]
+-- λ> count [isSpace, isDigit, isLetter]
+-- λ> 3
+
+
