b0f2021dd05c09134ac3b482813f3ed94e97a87d
fResult <styxmaz@gmail.com>
2025-11-21 20:15:51 +0700

    Complete maze parser by implementing charToTile.

---
+++ haskell/crash-course-2025/day-6/maze-v1.hs
@@ -0,0 +1,27 @@
+module MazeV1 where
+
+data Tile = Wall | Empty | Start | Goal deriving (Show, Eq)
+data Direction = North | East | South | West deriving (Show, Eq)
+type Position = (Int, Int)
+type Maze = [[Tile]]
+
+parseMap :: String -> Maze
+parseMap input = map parseLine (lines input)
+  where
+    parseLine [] = []
+    parseLine str =
+      case break (==']') str of
+        ('[':c:_, ']':rest) -> charToTile c : parseLine rest
+        _                   -> []
+
+    charToTile c =
+      case c of
+        'x' -> Wall
+        '_' -> Empty
+        's' -> Start
+        'o' -> Goal
+        _   -> Wall
+
+--- Dummy (Test Data) ---
+testMazeInput :: String
+testMazeInput = "[x][x][x][x][x][x][x][x]\n[x][x][x][_][_][x][x][x]\n[s][_][_][_][x][x][o][x]\n[x][x][x][_][x][x][_][x]\n[x][x][x][_][_][_][_][x]\n[x][x][x][x][x][x][x][x]\n"
