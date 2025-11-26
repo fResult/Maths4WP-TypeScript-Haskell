a72898f8f7b526fa1c9edd20123fa18a864aa0b8
fResult <styxmaz@gmail.com>
2025-11-26 19:06:57 +0700

    Implement `char` parser primitive
    - Enable `LambdaCase` language extension to simplify pattern matching syntax
    - Define `char` parser to match and consume a specific character from the input
    - Return `Nothing` safely if the input is empty or the character does not match

--- haskell/crash-course-2025/day-6/ParserV1.hs
+++ haskell/crash-course-2025/day-6/ParserV1.hs
@@ -1,3 +1,14 @@
+{-# LANGUAGE LambdaCase #-}
 module ParserV1 where
 
 newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
+
+-- char :: Char -> Parser Char
+-- char c = Parser (\input ->
+--   case input of
+--     (x:xs) | x == c    -> Just(x, xs)
+--     _                  -> Nothing)
+char :: Char -> Parser Char
+char c = Parser (\case
+  (x : xs) | x == c -> Just (x, xs)
+  _ -> Nothing)
