6584fa88a23e7e1c0b397f8250ae0d45aff71c5a
fResult <styxmaz@gmail.com>
2026-05-25 00:08:57 +0700

    Implement custom `State` monad instances in `Stack.hs`

--- haskell/crash-course-2025/day-7/Stack.hs
+++ haskell/crash-course-2025/day-7/Stack.hs
@@ -24,3 +24,36 @@ manipulateStack s0 =
       ((), s2) = push 3 s1
       ((), s3) = push 4 s2
   in pop s3
+
+newtype State' s a = State' { runState' :: s -> (a, s) }
+
+instance Functor (State' s) where
+  fmap :: (a -> b) -> State' s a -> State' s b
+  fmap f (State' g) = State' $ \s ->
+    let (a, s') = g s
+    in (f a, s')
+
+instance Applicative (State' s) where
+  pure :: a -> State' s a
+  pure x = State' { runState' = \s -> (x, s) }
+
+  (<*>) :: State' s (a -> b) -> State' s a -> State' s b
+  (State' hf) <*> (State' hg) = State' $ \s ->
+    let (f, s')  = hf s
+        (x, s'') = hg s'
+    in (f x, s'')
+
+instance Monad (State' s) where
+  (>>=) :: State' s a -> (a -> State' s b) -> State' s b
+  (State' h) >>= f = State' $ \s ->
+    let (a, s')    = h s
+        (State' g) = f a
+    in g s'
+
+class Monad m => MonadState' s m where
+  state' :: (s -> (a, s)) -> m a
+
+-- state' helper
+instance MonadState' s (State' s) where
+  state' :: (s -> (a, s)) -> State' s a
+  state' f = State' { runState' = f }
